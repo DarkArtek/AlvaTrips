06da0c8044fed0208b2552d087ff7578204aab77
Luca <luca@ahd-creative.agency>
2024-05-19 07:15:29 +0200

    feat(elements): trip admin controller

---
+++ Http/Controllers/Admin/TripsAdminController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Modules\AlvaTrips\Http\Controllers\Admin;
+
+use App\Contracts\Controller;
+use Illuminate\Http\Request;
+
+/**
+ * Admin controller
+ */
+class TripsAdminController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @param Request $request
+     *
+     * @return mixed
+     */
+    public function index(Request $request)
+    {
+        return view('alvatrips::admin.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @param Request $request
+     *
+     * @return mixed
+     */
+    public function create(Request $request)
+    {
+        return view('alvatrips::admin.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     */
+    public function store(Request $request)
+    {
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param Request $request
+     *
+     * @return mixed
+     */
+    public function edit(Request $request)
+    {
+        return view('alvatrips::admin.edit');
+    }
+
+    /**
+     * Show the specified resource.
+     *
+     * @param Request $request
+     *
+     * @return mixed
+     */
+    public function show(Request $request)
+    {
+        return view('alvatrips::admin.show');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     */
+    public function update(Request $request)
+    {
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Request $request
+     */
+    public function destroy(Request $request)
+    {
+    }
+}
