34565d5dd137dd39d7a26abda9126f2cb6a48155
Luca <luca@ahd-creative.agency>
2024-05-20 07:18:38 +0200

    feat(element): route service provider
    Provides the routing of the application. Normal user and admin.

---
+++ Providers/RouteServiceProvider.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Modules\AlvaTrips\Providers;
+
+use Illuminate\Routing\Router;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Route;
+use Modules\AlvaTrips\Http\Controllers\Admin\TripsAdminController;
+use Modules\AlvaTrips\Http\Controllers\Admin\TripTemplatesAdminController;
+use Modules\AlvaTrips\Http\Controllers\Frontend\IndexController;
+
+/**
+ * Register the routes required for your module here
+ */
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * The root namespace to assume when generating URLs to actions.
+     *
+     * @var string
+     */
+    protected $namespace = 'Modules\AlvaTrips\Http\Controllers';
+
+    /**
+     * Called before routes are registered.
+     *
+     * Register any model bindings or pattern based filters.
+     *
+     * @param Router $router
+     * @return void
+     */
+    public function before(Router $router)
+    {
+        //
+    }
+
+    /**
+     * Define the routes for the application.
+     *
+     * @param Router $router
+     *
+     * @return void
+     */
+    public function map(Router $router)
+    {
+        $this->registerWebRoutes();
+        $this->registerAdminRoutes();
+    }
+
+    /**
+     *
+     */
+    protected function registerWebRoutes(): void
+    {
+        $config = [
+            'as'         => 'alvatrips.',
+            'namespace'  => $this->namespace . '\Frontend',
+            'middleware' => ['web']
+        ];
+
+        Route::group($config, function () {
+            Route::group(['middleware' => 'auth', 'prefix' => 'trips'], function () {
+                Route::get('/', 'IndexController@index')->name('index');
+                Route::get('/create', 'IndexController@create')->name('create');
+                Route::post('/', 'IndexController@store')->name('store');
+                Route::get('/{trip}', [IndexController::class, 'show'])->name('show');
+            });
+        });
+    }
+
+    protected function registerAdminRoutes(): void
+    {
+        $config = [
+            'as'         => 'admin.',
+            'prefix'     => 'admin/',
+            'namespace'  => $this->namespace . '\Admin',
+            'middleware' => ['web', 'role:admin'],
+        ];
+
+        Route::group($config, function () {
+            Route::group(['as' => 'trips.', 'prefix' => 'trips/'], function () {
+                Route::get('/', [TripsAdminController::class, 'index']);
+                Route::get('create', [TripsAdminController::class, 'create']);
+                Route::post('/', [TripsAdminController::class, 'store']);
+                Route::group(['prefix' => '/templates', 'as' => 'templates.'], function () {
+                    Route::get('/', [TripTemplatesAdminController::class, 'index']);
+                    Route::post('/', [TripTemplatesAdminController::class, 'store']);
+                    Route::get('/create', [TripTemplatesAdminController::class, 'create']);
+                    Route::get('/{id}', [TripTemplatesAdminController::class, 'show']);
+                    Route::get('/{id}/edit', [TripTemplatesAdminController::class, 'edit']);
+                    Route::patch('/{id}', [TripTemplatesAdminController::class, 'update']);
+                    Route::delete('/{id}', [TripTemplatesAdminController::class, 'destroy']);
+                });
+                Route::get('/{id}', [TripsAdminController::class, 'show']);
+                Route::get('/{id}/edit', [TripsAdminController::class, 'edit']);
+            });
+            Route::group(['as' => 'missions.', 'prefix' => 'missions/'], function () {
+
+            });
+        });
+    }
+}
